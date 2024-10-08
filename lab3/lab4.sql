3faddb45ae8dc055d7c1aa2569cedb9073be8443
senator17 <amoka8707@gmail.com>
2024-10-08 10:40:56 +0500

    d

--- lab3/lab4.sql
+++ lab3/lab4.sql
@@ -1,22 +1,35 @@
-INSERT INTO Warehouses (code, location, capacity)
-VALUES
-(1, 'Chicago', 3),
-(2, 'Chicago', 4),
-(3, 'New York', 7),
-(4, 'Los Angeles', 2),
-(5, 'San Francisco', 8);
+--1 
+select * from warehouses;
 
+--2
+select * from boxes where value >150;
 
-INSERT INTO Boxes (code, contents, value, warehouse)
-VALUES
-('0MN7', 'Rocks', 180, 3),
-('4H8P', 'Rocks', 250, 1),
-('4RT3', 'Scissors', 190, 4),
-('7G3H', 'Rocks', 200, 1),
-('8JN6', 'Papers', 75, 1),
-('8Y6U', 'Papers', 50, 3),
-('9J6F', 'Papers', 175, 2),
-('LL08', 'Rocks', 140, 4),
-('P0H6', 'Scissors', 125, 1),
-('P2T6', 'Scissors', 150, 2),
-('TU55', 'Papers', 90, 5);
+--3
+select distinct contents from boxes;
+
+--4
+select warehouse, count(*) as num from boxes group by warehouse;
+
+--5
+select warehouse, count(*) as num from boxes group by warehouse 
+having count(*) > 2;
+
+--6
+insert into warehouses(code, location, capacity)
+values(6, 'New York', 3);
+
+--7
+insert into boxes(code, contents, value, warehouse)
+values('H5RT', 'Papers', 200, 2);
+
+--8
+update boxes set value = value * 0.85
+where value = (select value from boxes order by valuse desc offset 2 limit 1)
+
+--9
+delete from boxes where value < 150;
+
+--10
+delete from boxes
+where warehouse in (select code from warehouses where location = 'New York')
+returning *;
