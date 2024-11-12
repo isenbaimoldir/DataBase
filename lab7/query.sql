51ccfcb8b93aec8d40587634e87011a10d838804
senator17 <amoka8707@gmail.com>
2024-11-12 15:36:18 +0500

    d

--- lab7/query.sql
+++ lab7/query.sql
@@ -1,33 +1,52 @@
--- 1
-CREATE INDEX idx_countries_name ON countries(name);
+CREATE DATABASE lab7;
 
--- Example
-SELECT * FROM countries WHERE name = 'Kazakhstan';
 
--- 2
-CREATE INDEX idx_employees_name_surname ON employees(name, surname);
+CREATE TABLE countries(
+    country_id SERIAL PRIMARY KEY,
+    name VARCHAR(25)
+);
 
--- Example Query
-SELECT * FROM employees WHERE name = 'Alice' AND surname = 'Johnson';
+CREATE TABLE departments(
+    department_id SERIAL PRIMARY KEY,
+    department_name VARCHAR(50) UNIQUE,
+    budget INT
+);
+
+CREATE TABLE employees(
+    employee_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(50),
+    phone_number VARCHAR(20),
+    salary INT,
+    department_id INT REFERENCES departments
+);
+
+--1
+CREATE INDEX name_index ON countries(name);
+SELECT * FROM countries WHERE name = 'string';
+
+--2
+CREATE INDEX employee_name_index ON employees(first_name, last_name);
+
+    SELECT * FROM employees WHERE first_name = 'string'
+    AND last_name = 'string';
 
 --3
-CREATE UNIQUE INDEX idx_employees_salary ON employees(salary);
+   CREATE UNIQUE INDEX employee_id_index ON employees(employee_id);
 
--- Example Query
-SELECT * FROM employees WHERE salary < 90000 AND salary > 60000;
+    SELECT * FROM employees WHERE salary < 10000000
+    AND salary > 5000000;
 
 --4
-CREATE INDEX idx_employees_name_substring ON employees((substring(name FROM 1 FOR 4)));
+CREATE INDEX sub_name_index ON employees(substring(first_name FROM 1 FOR 4));
 
--- Example Query
-SELECT * FROM employees WHERE substring(name FROM 1 FOR 4) = 'Jack';
-
-
+SELECT * FROM employees WHERE substring(first_name
+                                        from 1 for 4) = 'abcd';
 --5
-CREATE INDEX idx_departments_budget ON departments(budget);
-CREATE INDEX idx_employees_department_salary ON employees(department_id, salary);
+CREATE INDEX budget_index ON departments(budget);
+CREATE INDEX salary_index ON employees(salary);
 
--- Example Query
-SELECT * FROM employees e
-JOIN departments d ON d.department_id = e.department_id
-WHERE d.budget > 1000000 AND e.salary < 90000;
+SELECT * FROM employees e JOIN departments d
+ON d.department_id = e.department_id
+WHERE d.budget > 5000000 AND e.salary < 45060000;
